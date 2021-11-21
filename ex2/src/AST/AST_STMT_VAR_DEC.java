ac0ea18180c27f52a90bc65863bc678d6aadf70d
bloch <nblochn@gmail.com>
2021-11-21 23:58:18 +0200

    continue

--- ex2/src/AST/AST_STMT_VAR_DEC.java
+++ ex2/src/AST/AST_STMT_VAR_DEC.java
@@ -51,22 +51,4 @@ public class AST_STMT_VAR_DEC extends AST_STMT{
         /****************************************/
         AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,vd.SerialNumber);
     }
-
-//    public AST_STMT_VAR_DEC(AST_VAR_DEC vd)
-//    {
-//        /******************************/
-//        /* SET A UNIQUE SERIAL NUMBER */
-//        /******************************/
-//        SerialNumber = AST_Node_Serial_Number.getFresh();
-//
-//        /***************************************/
-//        /* PRINT CORRESPONDING DERIVATION RULE */
-//        /***************************************/
-//        System.out.format("====================== stmt -> varDec\n");
-//
-//        /*******************************/
-//        /* COPY INPUT DATA EXP ... */
-//        /*******************************/
-//        this.vd = vd;
-//    }
 }
