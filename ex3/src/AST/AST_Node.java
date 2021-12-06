e5d031047da2c72383b2cc79d9411c101c54e852
shayf123 <shayfux56@gmail.com.com>
2021-12-06 16:14:07 +0200

    add isT1SubInstanceT2 func

--- ex3/src/AST/AST_Node.java
+++ ex3/src/AST/AST_Node.java
@@ -29,6 +29,66 @@ public abstract class AST_Node
 		System.out.println("NO GetSignatures(AST_NODE) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); return null;
 	}
 
+
+	// parameters : t1 , t2 are both TYPE_CLASS (check before using this func)
+	// t1 is the function parameter , and t2 is the class field we search for
+	// return true iff t1 is subclass of t2
+	// tmp objects for grasp?
+	public boolean isT1SubInstanceT2_helper(TYPE_CLASS t1 , TYPE_CLASS t2){
+		if (t1.name.equals(t2.name)){//type ar equal
+			return true;
+		}
+		if (t2.father == null){
+			return false;
+		}
+		return isT1SubInstanceT2(t1 , t2.father);
+	}
+
+	public boolean isT1SubInstanceT2(TYPE t1 , TYPE t2){
+		//primitive type check , is nill primtive type also by grammer?
+		if (t1 instanceof TYPE_INT){
+			if (t2 instanceof TYPE_INT){
+				return true;
+			}
+			return false;
+		}
+		if (t1 instanceof TYPE_STRING){
+			if (t2 instanceof TYPE_STRING){
+				return true;
+			}
+			return false;
+		}
+		if (t1 instanceof TYPE_VOID){ // TODO : check if needed
+			if (t2 instanceof TYPE_VOID){
+				return true;
+			}
+			return false;
+		}
+		if (t1 instanceof TYPE_NIL){
+			if (t2 instanceof TYPE_ID){
+				TYPE_ID tmp_t2 = (TYPE_ID) t2;
+				if (tmp_t2.type.isClass() || tmp2_t2.type.isArray()){ //TODO : what about isFunction?
+					return true;
+				}
+				return false;
+			}
+		}
+		if (t1 instanceof TYPE_ARRAY && t2 instanceof TYPE_ARRAY){
+			if (t1.name.equals(t2.name)){ // noninterchangable (page 4-5 in pdf)
+				return true;
+			}
+			return false;
+		}
+		if (t1 instanceof TYPE_CLASS){
+			if (t2 instanceof TYPE_CLASS){
+				return isT1SubInstanceT2_helper((TYPE_CLASS) t1, (TYPE_CLASS) t2);
+			}
+			return false;
+		}
+		System.out.println("reach non reachable code in isT1SubInstanceT2 in AST_NODE");
+		return false;
+	}
+
 	public TYPE GetSignature(AST_TYPE t) {
 		TYPE return_type = null;
 		if (t instanceof AST_TYPE_INT) {
